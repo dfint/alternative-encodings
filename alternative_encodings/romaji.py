7bf26a458e0c95625ff0dbb77c6982c6d0285964
insolor <2442833+insolor@users.noreply.github.com>
2024-05-15 22:32:38 +0300

    Add experimental romaji encoding

---
+++ alternative_encodings/romaji.py
@@ -0,0 +1,76 @@
+import codecs
+import re
+from typing import Iterator
+
+import encodings.cp437 as cp437
+import cutlet
+
+katsu = cutlet.Cutlet()
+
+
+def is_translatable(s: str):
+    return katsu.romaji(s).strip() != s.strip()
+
+
+def translate_with_spaces(s: str) -> Iterator[str]:
+    start, middle, end = re.search(r"^(\s*)(.*?)(\s*)$", s).groups()
+    yield start
+    yield katsu.romaji(middle)
+    yield end
+
+
+def iter_translate(input: str) -> Iterator[str]:
+    for line_with_ending in input.splitlines(keepends=True):
+        line, end = re.search(r"^(.*?)([\r\n]*)$", line_with_ending, flags=re.MULTILINE + re.DOTALL).groups()
+        if line:
+            parts = re.split(r"(\b[\w \,]+\b)", line)
+            for part in parts:
+                yield from translate_with_spaces(part)
+
+        yield end
+
+
+def encode(input: str) -> str:
+    return "".join(iter_translate(input))
+
+
+class Codec(cp437.Codec):
+    def encode(self, input, errors="strict"):
+        return super().encode(encode(input), errors)
+
+
+class IncrementalEncoder(cp437.IncrementalEncoder):
+    def encode(self, input, final=False):
+        return super().encode(encode(input), final)
+
+
+IncrementalDecoder = cp437.IncrementalDecoder
+
+# encodings module API
+codec = Codec()
+
+regentry = codecs.CodecInfo(
+    name="romaji",
+    encode=codec.encode,
+    decode=codec.decode,
+    incrementalencoder=IncrementalEncoder,
+    incrementaldecoder=IncrementalDecoder,
+)
+
+
+def search_function(encoding):
+    if regentry.name == encoding:
+        return regentry
+
+    return None
+
+
+def register():
+    codecs.register(search_function)
+
+
+def unregister():
+    try:
+        codecs.unregister(search_function)
+    except AttributeError:
+        pass
---
+++ tests/test_romaji.py
@@ -0,0 +1,34 @@
+import codecs
+
+import pytest
+from utils import register_codec
+
+from alternative_encodings import romaji
+
+
+@pytest.fixture(scope="module", autouse=True)
+def register_codec_fixture():
+    with register_codec(romaji):
+        yield
+
+
+source_data = """
+"Go to Combat Training","戦闘訓練へ"
+"Organize Combat Training","戦闘訓練を組織する"
+"""
+
+encoded = b"""
+"Go to Combat Training","Sentou kunren e"
+"Organize Combat Training","Sentou kunren wo soshiki suru"
+"""
+
+
+@pytest.mark.parametrize("source_data, encoded",
+    [
+        (source_data, encoded),
+        ('\r\n', b'\r\n'),
+        ('吾輩は猫である。 名前はまだ無い。', b'Wagahai wa neko de aru. Namae wa mada nai.')
+    ]
+)
+def test_encode(source_data, encoded):
+    assert codecs.encode(source_data, "romaji") == encoded
